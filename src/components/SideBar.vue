5d523c957f911db9ee3becc1ee1d71f60abc4c1e
FAIRDEVX <115607341+FAIRDEVX@users.noreply.github.com>
2023-08-30 13:33:55 +0700

    update

---
+++ src/components/SideBar.vue
@@ -0,0 +1,181 @@
+<template>
+    <aside :class="`${is_expanded && 'is-expanded'}`">
+        <div class="logo">
+            <img src="../assets/vue.svg" alt="vue">
+        </div>
+
+        <div class="menu-toggle-warp">
+            <button class="menu-toggle" @click="ToggleMenu">
+                <span class="material-icons">toc</span>
+            </button>
+        </div>
+
+        <h3>Menu</h3>
+
+        <div class="menu">
+            <router-link class="button" to="/">
+                <span class="material-icons">home</span>
+                <span class="text">Main</span>
+            </router-link>
+
+            <router-link class="button" to="/about">
+                <span class="material-icons">chrome_reader_mode</span>
+                <span class="text">Courses</span>
+            </router-link>
+
+            <router-link class="button" to="/">
+                <span class="material-icons">today</span>
+                <span class="text">Schedule</span>
+            </router-link>
+
+            <router-link class="button" to="/">
+                <span class="material-icons">import_contacts</span>
+                <span class="text">Materials</span>
+            </router-link>
+
+            <router-link class="button" to="/">
+                <span class="material-icons">card_membership</span>
+                <span class="text">Certificates</span>
+            </router-link>
+
+            <router-link class="button" to="/">
+                <span class="material-icons">question_answer</span>
+                <span class="text">Messages</span>
+            </router-link>
+
+            <router-link class="button" to="/">
+                <span class="material-icons">settings</span>
+                <span class="text">Setting</span>
+            </router-link>
+
+        </div>
+    </aside>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const is_expanded = ref(false)
+
+const ToggleMenu = () => {
+    is_expanded.value = !is_expanded.value
+}
+</script>
+
+<style lang="scss" scoped>
+aside {
+    display: flex;
+    flex-direction: column;
+    width: calc(2rem + 32px);
+    min-height: 100vh;
+    overflow: hidden;
+    padding: 1rem;
+
+    background-color: var(--dark);
+    color: var(--light);
+
+    .logo {
+        margin-bottom: 1rem;
+
+        img {
+            width: 2rem;
+        }
+    }
+
+    .menu-toggle-warp {
+        display: flex;
+        justify-content: flex-end;
+        margin-bottom: 1rem;
+
+        position: relative;
+        top: 0;
+        transition: 0.2s ease-out;
+
+        .menu-toggle {
+            transition: 0.2s ease-out;
+
+            .material-icons {
+                font-size: 2rem;
+                color: var(--light);
+                transition: 0.2s ease-out;
+            }
+
+            &:hover {
+                .material-icons {
+                    color: var(--primary);
+                    transform: translateX(0.5rem);
+                }
+            }
+        }
+    }
+
+    h3,
+    .button .text {
+        opacity: 0;
+        transition: 0.3s ease-out;
+    }
+
+    .menu {
+        margin: 0 -1rem;
+
+        .button {
+            display: flex;
+            align-items: center;
+            text-decoration: none;
+
+            padding: 0.5rem 1rem;
+            transition: 0.2s ease-out;
+
+            .material-icons {
+                font-size: 2rem;
+                color: var(--light);
+                transition: 0.2s ease-out;
+            }
+
+            .text {
+                color: var(--light);
+                transition: 0.2s ease-out;
+            }
+
+            &:hover {
+                background-color: var(--dark-alt);
+
+                .material-icons .text {
+                    color: var(--primary);
+                }
+            }
+
+        }
+    }
+
+    &.is-expanded {
+        width: var(--sidebar-width);
+
+        .menu-toggle-warp {
+            top: -3rem;
+
+            .menu-toggle {
+                transform: rotate(-180deg);
+            }
+        }
+
+        h3,
+        .button .text {
+            opacity: 1;
+        }
+
+        .button {
+            .material-icons {
+                margin-right: 1rem;
+            }
+        }
+    }
+
+    transition: 0.2s ease-out;
+
+    @media (max-width: 768px) {
+        position: fixed;
+        z-index: 99;
+    }
+}
+</style>
